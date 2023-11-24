462eb4fa5af56fde8402f74caaceb644d5a72c65
Kevin Liu <kevin07liu@gmail.com>
2023-11-25 00:03:17 +0800

    feat: gpt solutions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>This time? Again</h1>
+      <h1>GPT works???</h1>
       <hr />
       <MarketingApp />
     </div>
