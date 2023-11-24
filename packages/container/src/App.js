d428ab56cd1312520390742bb52579ba29b284ea
Kevin Liu <kevin07liu@gmail.com>
2023-11-24 23:54:04 +0800

    feat: change region

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>This is final one</h1>
+      <h1>This time?</h1>
       <hr />
       <MarketingApp />
     </div>
