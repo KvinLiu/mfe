7d15dca2e0a4ffe7f3333d6fe4ec11f365bc94aa
Kevin Liu <kevin07liu@gmail.com>
2023-11-24 23:51:17 +0800

    feat: final update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>All Methods I've tried</h1>
+      <h1>This is final one</h1>
       <hr />
       <MarketingApp />
     </div>
