b2d15a5276dd4ddbd472dde61041c10a99ca05c4
Kevin Liu <kevin07liu@gmail.com>
2023-11-25 00:09:23 +0800

    feat: late night

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>GPT works???</h1>
+      <h1>GPT sucks!</h1>
       <hr />
       <MarketingApp />
     </div>
