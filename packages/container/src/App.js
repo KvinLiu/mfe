23de13b52c85882044138edebe789d7376b4a981
Kevin Liu <kevin07liu@gmail.com>
2023-12-02 15:33:10 +0800

    feat: add lazy loading and progress bar

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import Progress from './components/Progress';
 import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
+
+const MarketingLasy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -15,10 +18,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route exact path="/" component={MarketingApp} />
-            <Route path="/auth" component={AuthApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route exact path="/" component={MarketingLasy} />
+              <Route path="/auth" component={AuthLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
