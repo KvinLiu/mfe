dd1a2d614babfed4ff0f7cbb6d4ab3b9c8e8d817
Kevin Liu <kevin07liu@gmail.com>
2023-12-02 11:01:32 +0800

    feat: intergrade auth to container and fix public path issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import MarketingApp from './components/MarketingApp';
+import AuthApp from './components/AuthApp';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -14,7 +15,10 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            <Route exact path="/" component={MarketingApp} />
+            <Route path="/auth" component={AuthApp} />
+          </Switch>
         </div>
       </StylesProvider>
     </BrowserRouter>
